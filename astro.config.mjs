7e46ed0ab01ef8ad83da8e1e960164c54e2fefa8
khalatevarun <khalatevarun@gmail.com>
2025-11-28 14:32:40 -0600

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import vercel from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.varunhnk.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 })
